19d646b9ced5240c409a00053b4f77f1a5ebc635
rtritto <ariele.1994@gmail.com>
2025-04-06 15:28:46 +0200

    Remove title

--- src/pages/+config.ts
+++ src/pages/+config.ts
@@ -3,8 +3,7 @@ import type { Config } from 'vike/types'
 
 // Default config (can be overridden by pages)
 export default {
-  title: 'My Vike + Solid App', // <title>
-  description: 'Demo showcasing Vike + Solid', // <meta name='description'>
+  description: 'Mongo PWA GUI', // <meta name='description'>
   prerender: true,
   extends: vikeSolid
 } satisfies Config
