f2b43c6db61abbf681179a253ddc02643a67c1ca
rtritto <ariele.1994@gmail.com>
2025-10-09 23:20:23 +0200

    style: fix margin in DeleteDialog

--- src/components/common/DeleteDialog.tsx
+++ src/components/common/DeleteDialog.tsx
@@ -31,14 +31,18 @@ const DeleteDialog: Component<{
           <h3 class="text-lg font-bold">{props.title} <b>"{props.value}"</b></h3>
 
           <form onSubmit={async (event) => event.preventDefault()  /* Disable page reload after submit */}>
-            <p class="text-sm m-1">{props.message}</p>
+            <div class="m-2">
+              <p class="text-sm">{props.message}</p>
+            </div>
 
             <Show when={props.enableInput}>
-              <input class="input m-3 w-full" type="text" placeholder={`Type "${props.value}"`} value={input()} onKeyUp={(event) => setInput(event.currentTarget.value.trim())} />
+              <div class="m-2">
+                <input class="input w-full" type="text" placeholder={`Type "${props.value}"`} value={input()} onKeyUp={(event) => setInput(event.currentTarget.value.trim())} />
+              </div>
             </Show>
 
-            <div class="text-center">
-              <button class="btn bg-red-700 m-1 py-0.5" type="submit" onClick={() => props.handleDelete(input())} disabled={props.enableInput ? input() !== props.value : false}>
+            <div class="text-center m-2">
+              <button class="btn bg-red-700 py-0.5" type="submit" onClick={() => props.handleDelete(input())} disabled={props.enableInput ? input() !== props.value : false}>
                 Delete
               </button>
             </div>
