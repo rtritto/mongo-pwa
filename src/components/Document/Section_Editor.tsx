b91bc1a8776b655a6d37bc24ee6a9d9573032d6c
rtritto <ariele.1994@gmail.com>
2025-10-10 16:32:25 +0200

    refactor: Back and Delete Buottons in View Document

--- src/components/Document/Section_Editor.tsx
+++ src/components/Document/Section_Editor.tsx
@@ -1,8 +1,11 @@
 import { createSignal, onMount, type Component } from 'solid-js'
+import { navigate } from 'vike/client/router'
 
+import DeleteDocument from '@/components/Collection/DeleteDocument'
 import EditorCodeMirror from './EditorCodeMirror'
+import IconBack from '@/components/Icons/IconBack'
 
-const Section_Editor: Component<{ docString: string, readOnly: boolean }> = (props) => {
+const Section_Editor: Component<{ data: DataDocument }> = (props) => {
   let containerRef: HTMLDivElement | undefined
   let hiddenTextarea: HTMLTextAreaElement | undefined
 
@@ -11,14 +14,37 @@ const Section_Editor: Component<{ docString: string, readOnly: boolean }> = (pro
   onMount(() => {
     // Create hidden textarea to use in editor factory
     hiddenTextarea = document.createElement('textarea')
-    hiddenTextarea.value = props.docString
+    hiddenTextarea.value = props.data.docString
     containerRef!.append(hiddenTextarea)
 
-    setView(EditorCodeMirror(hiddenTextarea, { readOnly: props.readOnly }))
+    setView(EditorCodeMirror(hiddenTextarea, { readOnly: props.data.readOnly }))
   })
 
   return (
-    <div ref={containerRef}>{view()}</div>
+    <div>
+      <div ref={containerRef}>{view()}</div>
+
+      <div class="m-2">
+        <button class="btn btn-sm bg-yellow-500 py-0.5 text-right" onClick={async () => {
+          await navigate(`/db/${props.data.selectedDatabase}/${props.data.selectedCollection}`)
+        }}>
+          <IconBack />
+
+          Back
+        </button>
+      </div>
+
+      <div class="m-2">
+        <DeleteDocument
+          database={props.data.selectedDatabase}
+          collection={props.data.selectedCollection}
+          _id={props.data._id}
+          sub_type={props.data.subtype}
+          doReload={false}
+          showLabel={true}
+        />
+      </div>
+    </div>
   )
 }
 
--- src/pages/db/@dbName/@collectionName/@document/+Page.tsx
+++ src/pages/db/@dbName/@collectionName/@document/+Page.tsx
@@ -1,10 +1,7 @@
 import type { Component } from 'solid-js'
 import { useData } from 'vike-solid/useData'
-import { navigate } from 'vike/client/router'
 
-import DeleteDocument from '@/components/Collection/DeleteDocument'
 import Section_Editor from '@/components/Document/Section_Editor'
-import IconBack from '@/components/Icons/IconBack'
 
 const Page: Component<DataDocument> = () => {
   const [data] = useData<DataDocument>()
@@ -12,29 +9,7 @@ const Page: Component<DataDocument> = () => {
     <>
       <h1>{data.title}</h1>
 
-      <Section_Editor docString={data.docString} readOnly={data.readOnly} />
-
-      <div class="m-2">
-        <button class="btn btn-sm bg-yellow-500 py-0.5 text-right" onClick={async () => {
-          await navigate(`/db/${data.selectedDatabase}/${data.selectedCollection}`)
-        }}>
-          <IconBack />
-
-          Back
-        </button>
-      </div>
-
-
-      <div class="m-2">
-        <DeleteDocument
-          database={data.selectedDatabase}
-          collection={data.selectedCollection}
-          _id={data._id}
-          sub_type={data.subtype}
-          doReload={false}
-          showLabel={true}
-        />
-      </div>
+      <Section_Editor data={data} />
     </>
   )
 }
