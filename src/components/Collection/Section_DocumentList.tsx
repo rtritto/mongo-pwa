f09404a887975274a1b94574081811e09781b640
rtritto <ariele.1994@gmail.com>
2025-10-10 15:20:59 +0200

    fix: subtype QP

--- src/components/Collection/Section_DocumentList.tsx
+++ src/components/Collection/Section_DocumentList.tsx
@@ -24,7 +24,10 @@ const Section_DocumentList: Component<{ data: DataCollection }> = (props) => {
             {(document) => (
               <tr>
                 <th>
-                  <a class="btn btn-sm bg-blue-500 m-1" href={`/db/${props.data.selectedDatabase}/${props.data.selectedCollection}/${document._id}`}>
+                  <a
+                    class="btn btn-sm bg-blue-500 m-1"
+                    href={`/db/${props.data.selectedDatabase}/${props.data.selectedCollection}/${document._id}${document.sub_type === undefined ? '' : `?subtype=${document.sub_type}`}`}
+                  >
                     <IconSearch />
                   </a>
 
