04e509bd38a7e87f88349224b8eaf0d52a693b9f
rtritto <ariele.1994@gmail.com>
2025-10-12 19:17:03 +0200

    refactor: improve fetchWithRetries

--- src/utils/fetchWithRetries.ts
+++ src/utils/fetchWithRetries.ts
@@ -1,26 +1,24 @@
-const fetchWithRetries = async (url: string, options = {}, retries = 5, delay = 1000) => {
+export default async function fetchWithRetries(
+  url: string,
+  options: RequestInit = {},
+  retries = 3,
+  delay = 500
+): Promise<Response> {
+  let error: unknown
+
   for (let i = 0; i < retries; i++) {
     try {
       const res = await fetch(url, options)
-
-      // Check for non-successful HTTP status codes
-      if (res.ok) {
-        // Successful fetch
-        return res
+      if (res.ok) return res
+      throw new Error(`HTTP ${res.status}`)
+    } catch (error_: unknown) {
+      error = error_
+      if (i < retries - 1) {
+        await new Promise((resolve) => setTimeout(resolve, delay * 2 ** i))
       }
-      throw new Error(`HTTP error! Status: ${res.status}`)
-    } catch (error: unknown) {
-      // console.error(`Attempt ${i + 1} failed: ${error}`)
-
-      // Stop retrying if the last attempt fails
-      if (i === retries - 1) {
-        throw new Error(`Fetch failed after ${retries} attempts: ${error}`)
-      }
-
-      // Wait for some time before retrying (exponential backoff)
-      await new Promise((resolve) => setTimeout(resolve, delay * Math.pow(2, i)))
     }
   }
-}
 
-export default fetchWithRetries
+  console.error('Fetch failed:', error)
+  throw new Error('Fetch failed')
+}
