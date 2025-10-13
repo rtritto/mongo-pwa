54c236a1982f6f868f50e425deb213a4077e2897
rtritto <ariele.1994@gmail.com>
2025-10-13 12:27:24 +0200

    refactor: console.debug to console.error

--- src/server/api/collectionCreate.ts
+++ src/server/api/collectionCreate.ts
@@ -10,7 +10,7 @@ export default async function collectionCreate(c: Context) {
   await connectClient()
   checkDatabase(database)
   await globalThis.mongo.mongoClient.db(database).createCollection(collection).catch((error) => {
-    console.debug(error)
+    console.error(error)
     throw new Error(`Failed to create collection. ${error.message}`)
   })
   return c.json({})
