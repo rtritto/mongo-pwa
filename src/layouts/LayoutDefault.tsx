949a17dfd1423f9936165b94b3678a1d85df8187
rtritto <ariele.1994@gmail.com>
2025-10-14 12:49:53 +0200

    fix: t.options not found in PROD

--- src/layouts/LayoutDefault.tsx
+++ src/layouts/LayoutDefault.tsx
@@ -12,7 +12,7 @@ export const LayoutDefault: Component<{ children?: JSX.Element }> = (props) => {
   const [password, setPassword] = createSignal<string | null>(null)
 
   onMount(() => {
-    if (data?.options.localStorageAuth.enabled) {
+    if (data?.options?.localStorageAuth.enabled) {
       setPassword(localStorage.getItem(data.options.localStorageAuth.localStorageAuthKey) || '')
     }
   })
@@ -31,7 +31,7 @@ export const LayoutDefault: Component<{ children?: JSX.Element }> = (props) => {
 
   return (
     <Show
-      when={!data?.options.localStorageAuth.enabled || password() === data?.options.localStorageAuth.localStorageAuthPassword}
+      when={!data?.options?.localStorageAuth.enabled || password() === data?.options?.localStorageAuth.localStorageAuthPassword}
       fallback={
         password() === null ? null : (
           <label>
