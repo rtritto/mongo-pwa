53b61282f91b1682a5ed24aae4cbe3d4b16af808
rtritto <ariele.1994@gmail.com>
2025-10-25 16:10:32 +0200

    feat: add validation to Upadate Doc

--- src/components/Document/Editor.tsx
+++ src/components/Document/Editor.tsx
@@ -1,10 +1,11 @@
-import { type Component, untrack } from 'solid-js'
+import { type Component, createSignal, untrack } from 'solid-js'
 import type { SetStoreFunction } from 'solid-js/store'
 
 import DeleteDocument from '@/components/Collection/DeleteDocument'
 import createCodeMirror from '@/components/common/createCodeMirror'
 import BackButton from './BackButton'
 import SaveButton from './SaveButton'
+import { toSafeBSON } from '@/utils/bson'
 
 const Editor: Component<{
   data: DataDocument
@@ -14,14 +15,20 @@ const Editor: Component<{
     untrack(() => props.data.docString),
     { readOnly: untrack(() => props.data.readOnly) }
   )
+  const [isTextValid, setIsTextValid] = createSignal(true)
 
   return (
     <div>
-      <div ref={editorRef} />
+      <div
+        ref={editorRef}
+        onKeyUp={() => {
+          setIsTextValid(!!toSafeBSON(editorView()!.state.doc.toString()))
+        }}
+      />
 
       <BackButton view={editorView()!} data={props.data} setData={props.setData} />
 
-      <SaveButton view={editorView()!} data={props.data} setData={props.setData} />
+      <SaveButton view={editorView()!} data={props.data} setData={props.setData} disabled={!isTextValid()} />
 
       <div class="m-2">
         <DeleteDocument
--- src/components/Document/SaveButton.tsx
+++ src/components/Document/SaveButton.tsx
@@ -11,29 +11,34 @@ const SaveButton: Component<{
   view: CustomEditorView
   data: DataDocument
   setData: SetStoreFunction<any>
+  disabled: boolean
 }> = (props) => {
   return (
-    <button class="btn btn-sm bg-green-500 py-0.5 text-right" onClick={async () => {
-      const response = await handleFetchError(
-        fetch('/api/documentUpdate', {
-          method: 'POST',
-          headers: HEADERS_JSON(props.data.options),
-          body: JSON.stringify({
-            database: props.data.selectedDatabase,
-            collection: props.data.selectedCollection,
-            doc: props.view.state.doc.toString(),
-            _id: props.data._id,
-            sub_type: props.data.subtype
-          })
-        }),
-        props.setData
-      )
-      if (response) {
-        await response.json() as { insertedId: string }
-        localStorage.setItem('me-success', `Document "${props.data._id}" updated!`)
-        await navigate(`/db/${props.data.selectedDatabase}/${props.data.selectedCollection}`)
-      }
-    }}>
+    <button
+      class="btn btn-sm bg-green-500 py-0.5 text-right"
+      disabled={props.disabled}
+      onClick={async () => {
+        const response = await handleFetchError(
+          fetch('/api/documentUpdate', {
+            method: 'POST',
+            headers: HEADERS_JSON(props.data.options),
+            body: JSON.stringify({
+              database: props.data.selectedDatabase,
+              collection: props.data.selectedCollection,
+              doc: props.view.state.doc.toString(),
+              _id: props.data._id,
+              sub_type: props.data.subtype
+            })
+          }),
+          props.setData
+        )
+        if (response) {
+          await response.json() as { insertedId: string }
+          localStorage.setItem('me-success', `Document "${props.data._id}" updated!`)
+          await navigate(`/db/${props.data.selectedDatabase}/${props.data.selectedCollection}`)
+        }
+      }}
+    >
       <IconSave />
 
       Save
