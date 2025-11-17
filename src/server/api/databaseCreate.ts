eb7625f915c6baf66da600ff88632a7a6af8213e
rtritto <ariele.1994@gmail.com>
2025-11-17 23:27:49 +0100

    fix: remove checkDatabase on databaseCreate API

--- src/server/api/databaseCreate.ts
+++ src/server/api/databaseCreate.ts
@@ -1,12 +1,10 @@
 import type { Context } from 'hono'
 
 import { connectClient } from '@/server/db'
-import { checkDatabase } from '@/utils/validationsServer'
 
 export default async function databaseDelete(c: Context) {
   const { database } = await c.req.json<{ database: string }>()
   await connectClient()
-  checkDatabase(database)
   await globalThis.mongo.mongoClient.db(database).createCollection('delete_me').catch((error) => {
     console.debug(error)
     throw new Error(`Failed to create collection. ${error.message}`)
