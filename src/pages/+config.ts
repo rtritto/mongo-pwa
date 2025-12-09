99c7d75b3a8220c0b4f4947e8b1711f39e519c44
rtritto <ariele.1994@gmail.com>
2025-12-09 17:12:38 +0100

    chore(deps): bump @photonjs/hono and vike-photon

--- src/pages/+config.ts
+++ src/pages/+config.ts
@@ -28,6 +28,7 @@ export default {
     standalone: {
       bundle: true,
       // minify: true
-    }
+    },
+    target: process.env.ENTRY_NODE === 'true' ? 'node' : undefined
   }
 } satisfies Config
