3dc6d2a19bfefb54f04ed6ba2ef4bf3865fa25a1
rtritto <ariele.1994@gmail.com>
2026-03-28 16:28:29 +0100

    refactor

--- src/utils/mappers/getColumnsAndSetDocs.ts
+++ src/utils/mappers/getColumnsAndSetDocs.ts
@@ -4,8 +4,12 @@ const getColumnsAndSetDocs = (docs: MongoDocument[]) => {
   for (const i in docs) {
     const currentColumns = Object.keys(docs[i])
     columns.push(currentColumns)
-    // Used by DELETE document
-    docs[i].sub_type = docs[i]._id.sub_type as number | undefined
+    // JSON.stringify will remove the "sub_type" field from the _id field:
+    // we need the "sub_type" to get the correct type and use DELETE document,
+    // so we set it on the root of the document
+    if ('sub_type' in docs[i]._id) {
+      docs[i].sub_type = docs[i]._id.sub_type as number
+    }
   }
 
   const noDuplicateColumns = columns.flat().filter((value, index, arr) => arr.indexOf(value) === index)  // Remove duplicates
