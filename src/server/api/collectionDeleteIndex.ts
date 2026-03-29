54f334bde75f075c3e2f2c302d4bbaca68a06f6f
rtritto <ariele.1994@gmail.com>
2026-03-29 14:01:12 +0200

    fix: add check options

--- src/server/api/collectionDeleteIndex.ts
+++ src/server/api/collectionDeleteIndex.ts
@@ -1,7 +1,7 @@
 import type { Context } from 'hono'
 
 import { connectClient } from '@/server/db'
-import { checkDatabaseCollection } from '@/utils/validations/serverChecks'
+import { checkDatabaseCollection, checkOptions } from '@/utils/validations/serverChecks'
 
 export default async function collectionDeleteIndex(c: Context) {
   const { database, collection, indexName } = await c.req.json<{
@@ -10,6 +10,13 @@ export default async function collectionDeleteIndex(c: Context) {
     indexName: string
   }>()
   await connectClient()
+  const { error: optionError } = checkOptions({
+    readOnly: false,
+    noDelete: false
+  })
+  if (optionError) {
+    return c.json({ error: optionError }, 403)
+  }
   const { error } = checkDatabaseCollection(database, collection)
   if (error) {
     return c.json({ error }, 404)
--- src/server/api/databaseDelete.ts
+++ src/server/api/databaseDelete.ts
@@ -1,10 +1,18 @@
 import type { Context } from 'hono'
 
 import { connectClient } from '@/server/db'
+import { checkOptions } from '@/utils/validations/serverChecks'
 
 export default async function databaseDelete(c: Context) {
   const { database } = await c.req.json<{ database: string }>()
   await connectClient()
+  const { error: optionError } = checkOptions({
+    readOnly: false,
+    noDelete: false
+  })
+  if (optionError) {
+    return c.json({ error: optionError }, 403)
+  }
   await globalThis.mongo.mongoClient.db(database).dropDatabase()
     .catch((error) => {
       console.debug(error)
