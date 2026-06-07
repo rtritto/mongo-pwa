14cd8fe405efdd833aa61c96495cf6ba753eedb2
rtritto <ariele.1994@gmail.com>
2026-06-07 23:37:56 +0200

    refactor: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
-import standaloner from 'standaloner/vite'
 import vike from 'vike/plugin'
 import vikeSolid from 'vike-solid/vite'
 import { defineConfig, loadEnv, type UserConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
+
+const minify = true
 
 export default defineConfig(async ({ mode }) => {
   // Add ME_CONFIG_ env vars to process.env
@@ -17,36 +17,38 @@ export default defineConfig(async ({ mode }) => {
       tailwindcss(),
       vike(),
       vikeSolid(),
-      standaloner({
-        bundle: true,
-        minify: true
-      }),
-      ...process.env.NODE_ENV === 'production' ? [VitePWA({
-        registerType: 'autoUpdate', // Automatically updates the service worker
-        devOptions: {
-          // enabled: true,  // Enable PWA in development mode ~ Disable https://github.com/vikejs/vike/issues/388#issuecomment-1199280084
-          type: 'module'
-        },
-        manifest: {
-          name: 'Mongo PWA',
-          short_name: 'MongoPWA',
-          theme_color: '#3F51B5',
-          background_color: '#3367D6',
-          icons: [
-            {
-              src: '/icons/logo-192.png',
-              sizes: '192x192',
-              type: 'image/png'
-            }
-          ]
-        }
-      }).map((plugin) => ({
-        ...plugin,
-        // Prevent from generating registerSW.js inside /dist/server/
-        applyToEnvironment(environment: { name: string }) {
-          return environment.name === 'client'
-        }
-      }))] : []
+      ...process.env.NODE_ENV === 'production' ? [
+        (await import('vite-plugin-pwa')).VitePWA({
+          registerType: 'autoUpdate', // Automatically updates the service worker
+          devOptions: {
+            // enabled: true,  // Enable PWA in development mode ~ Disable https://github.com/vikejs/vike/issues/388#issuecomment-1199280084
+            type: 'module'
+          },
+          manifest: {
+            name: 'Mongo PWA',
+            short_name: 'MongoPWA',
+            theme_color: '#3F51B5',
+            background_color: '#3367D6',
+            icons: [
+              {
+                src: '/icons/logo-192.png',
+                sizes: '192x192',
+                type: 'image/png'
+              }
+            ]
+          }
+        }).map((plugin) => ({
+          ...plugin,
+          // Prevent from generating registerSW.js inside /dist/server
+          applyToEnvironment(environment: { name: string }) {
+            return environment.name === 'client'
+          }
+        })),
+        (await import('standaloner/vite')).default({
+          bundle: true,
+          minify
+        })
+      ] : []
     ],
     server: {
       // host: '127.0.0.1',
@@ -56,12 +58,10 @@ export default defineConfig(async ({ mode }) => {
     build: {
       target: 'esnext',
       outDir: '../dist',
-      minify: true
+      emptyOutDir: true,
+      minify
     },
     envPrefix: 'ME_CONFIG_',
-    esbuild: {
-      legalComments: 'none'
-    },
     resolve: {
       alias: {
         '@': path.resolve(import.meta.dirname, 'src')
