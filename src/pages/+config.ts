6f17c57c309d360e8332bc78511870dfdb9d27db
rtritto <ariele.1994@gmail.com>
2026-06-08 12:35:15 +0200

    disable prerender

--- src/pages/+config.ts
+++ src/pages/+config.ts
@@ -3,9 +3,6 @@ import vikeSolid from 'vike-solid/config'
 
 // Default config (can be overridden by pages)
 export default {
-  prerender: {
-    partial: true
-  },
   // title: 'Mongo PWA', // <title>
   // description: 'Demo showcasing Vike + Solid', // <meta name='description'>
   // bodyAttributes: {
