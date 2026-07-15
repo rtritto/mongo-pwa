40e048e9deeec21be66234650afdaf7f45e08a84
rtritto <ariele.1994@gmail.com>
2026-07-15 17:24:09 +0200

    lint

--- src/server/apiRoutes.ts
+++ src/server/apiRoutes.ts
@@ -14,7 +14,7 @@ apiRoutes.post('/:functionName', async (c: Context) => {
     return c.json({ error: 'Not authenticated' }, 401)
   }
   const functionName = c.req.param('functionName') as keyof typeof api
-  if (!(functionName in api)) {
+  if (!Object.hasOwn(api, functionName)) {
     return c.json({ error: 'Function not found' }, 400)
   }
   try {
