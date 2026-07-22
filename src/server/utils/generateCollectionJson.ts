7d1678d419dc34d70f0a7066f8c97d7607831530
rtritto <ariele.1994@gmail.com>
2026-07-22 21:01:51 +0200

    fix: close cursor

--- src/server/utils/generateCollectionJson.ts
+++ src/server/utils/generateCollectionJson.ts
@@ -1,14 +1,20 @@
 import { EJSON } from 'bson'
+import type { AbstractCursor, Document } from 'mongodb'
 
 const encoder = new TextEncoder()
 
-export async function* generateCollectionJson(cursor: AsyncIterable<unknown>): AsyncGenerator<Uint8Array> {
+export async function* generateCollectionJson(cursor: AbstractCursor<Document>): AsyncGenerator<Uint8Array> {
   yield encoder.encode('[')
 
   let isFirst = true
-  for await (const doc of cursor) {
-    yield encoder.encode((isFirst ? '' : ',') + EJSON.stringify(doc))
-    isFirst = false
+
+  try {
+    for await (const doc of cursor) {
+      yield encoder.encode((isFirst ? '' : ',') + EJSON.stringify(doc))
+      isFirst = false
+    }
+  } finally {
+    await cursor.close()
   }
 
   yield encoder.encode(']')
